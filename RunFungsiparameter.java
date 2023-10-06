a0e255f72d31e86a607e9cfec566ae7277f44fd6
mah11labib <145730187+mah11labib@users.noreply.github.com>
2023-10-06 11:09:28 +0700

    Add files via upload

---
+++ RunFungsiparameter.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author pc20
+ */
+public class RunFungsiparameter {
+    public static void main(String [] args){
+        int alas = 8;
+        int tinggi = 6;
+        fungsiParameter Q = new fungsiParameter();
+        System.out.println("Luas Jajargenjang =" + Q.hitungLuasJajargenjang(alas, tinggi));
+    }
+}
---
+++ Runmaster.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan3;
+
+/**
+ *
+ * @author pc20
+ */
+public class Runmaster {
+   public static void main(String[] args) {
+      
+        String [] Merek = {"Samsung" ,"Lenovo" ,"Xiomi" ,"Oppo","Poco"};
+        
+        System.out.println(Merek[0]);
+        System.out.println(Merek[1]);
+        System.out.println(Merek[2]);
+        System.out.println(Merek[3]);
+        System.out.println(Merek[4]);
+        System.out.println("------------");
+            
+        }
+    }
+
+
